baf2069b08eb79188cb4d4b7a9818c5c4b814a74
toyknight <toyknight@outlook.com>
2015-09-21 15:46:53 -0400

    update connector for json support

--- app/src/main/java/com/njit/buddy/app/Connector.java
+++ app/src/main/java/com/njit/buddy/app/Connector.java
@@ -5,10 +5,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import java.io.*;
-import java.net.CookieManager;
-import java.net.HttpCookie;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.*;
 import java.util.List;
 
 /**
@@ -19,7 +16,7 @@ public class Connector {
     private static final String GET = "GET";
     private static final String POST = "POST";
     private static final String COOKIES_HEADER = "Set-Cookie";
-    private static final String DEBUG_TAG = "DEBUG";
+    private static final String DEBUG_TAG = "Network";
 
     private static CookieManager cookie_manager;
 
@@ -34,52 +31,70 @@ public class Connector {
      * Send a GET request.
      *
      * @param url the request url
+     * @param content the request body
      * @return the response content
      * @throws IOException
      */
-    public static String executeGet(String url) throws IOException {
+    public static String executeGet(String url, String content) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
         connection.setConnectTimeout(15000);
         connection.setReadTimeout(10000);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Accept", "*/*");
         connection.setRequestMethod(GET);
+        connection.setDoOutput(true);
         connection.setDoInput(true);
         attachCookie(connection);
         connection.connect();
-        int response = connection.getResponseCode();
-        Log.d(DEBUG_TAG, "The response code for [GET] '" + url + "' is " + response);
 
-        String content = getContent(connection.getInputStream());
+        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+        writer.write(content);
+        writer.flush();
+
+        int code = connection.getResponseCode();
+        Log.d(DEBUG_TAG, "The response code for [GET] '" + url + "' is " + code);
+
+        String response = getContent(connection);
         updateCookieStore(connection);
         connection.disconnect();
-        return content;
+        return response;
     }
 
     /**
      * Send a POST request.
      *
      * @param url the request url
+     * @param content the request body
      * @return the response content
      * @throws IOException
      */
-    public static String executePost(String url) throws IOException {
+    public static String executePost(String url, String content) throws IOException {
         HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
         connection.setConnectTimeout(15000);
         connection.setReadTimeout(10000);
+        connection.setRequestProperty("Content-Type", "application/json");
+        connection.setRequestProperty("Accept", "*/*");
         connection.setRequestMethod(POST);
+        connection.setDoOutput(true);
         connection.setDoInput(true);
         attachCookie(connection);
         connection.connect();
-        int response = connection.getResponseCode();
-        Log.d(DEBUG_TAG, "The response code for [POST] '" + url + "' is " + response);
 
-        String content = getContent(connection.getInputStream());
+        OutputStreamWriter writer = new OutputStreamWriter(connection.getOutputStream());
+        writer.write(content);
+        writer.flush();
+
+        int code = connection.getResponseCode();
+        Log.d(DEBUG_TAG, "The response code for [POST] '" + url + "' is " + code);
+
+        String response = getContent(connection);
         updateCookieStore(connection);
         connection.disconnect();
-        return content;
+        return response;
     }
 
-    private static String getContent(InputStream is) throws IOException {
-        InputStreamReader ir = new InputStreamReader(is, "UTF-8");
+    private static String getContent(URLConnection connection) throws IOException {
+        InputStreamReader ir = new InputStreamReader(connection.getInputStream(), "UTF-8");
         BufferedReader br = new BufferedReader(ir);
         String line;
         StringBuilder content = new StringBuilder();
